76dee3a6f43ae66b58fd13155fdedaf54d11f638
Braxton Mckee <braxton@aprioriinvestments.com>
2018-08-08 16:45:26 -0400

    Implement an asyncio protocol that uses Algebraics as a message type.

---
+++ object_database/algebraic_protocol.py
@@ -0,0 +1,59 @@
+import asyncio
+import struct
+import json
+import logging
+import traceback
+
+from object_database.algebraic_to_json import Encoder
+
+sizeType = '<L'
+longLength = struct.calcsize(sizeType)
+
+def longToString(l):
+    return struct.pack(sizeType, l)
+
+def stringToLong(l):
+    return struct.unpack(sizeType, l)[0]
+
+class AlgebraicProtocol(asyncio.Protocol):
+    def __init__(self, receiveType, sendType):
+        self.encoder = Encoder()
+        self.receiveType = receiveType
+        self.sendType = sendType
+        self.transport = None
+        self.buffer = bytes()
+
+    def sendMessage(self, msg):
+        assert isinstance(msg, self.sendType), "message %s is of type %s != %s" % (msg, type(msg), self.sendType)
+        dataToSend = bytes(json.dumps(self.encoder.to_json(self.sendType, msg)), 'utf8')
+        self.transport.write(longToString(len(dataToSend)) + dataToSend)
+
+    def messageReceived(self, msg):
+        #subclasses override
+        pass
+
+    def onConnected(self):
+        pass
+
+    def connection_made(self, transport):
+        self.transport = transport
+        self.onConnected()
+
+    def data_received(self, data):
+        assert isinstance(data, bytes)
+        self.buffer += data
+
+        while len(self.buffer) >= longLength:
+            bytesToRead = stringToLong(self.buffer[:longLength])
+            if bytesToRead + longLength <= len(self.buffer):
+                toConsume = self.buffer[longLength:bytesToRead + longLength]
+                self.buffer = self.buffer[bytesToRead + longLength:]
+
+                try:
+                    self.messageReceived(self.encoder.from_json(json.loads(str(toConsume,'utf8')), self.receiveType))
+                except:
+                    logging.info("Error in AlgebraicProtocol: %s", traceback.format_exc())
+                    self.transport.close()
+            else:
+                return
+
---
+++ object_database/algebraic_protocol_test.py
@@ -0,0 +1,72 @@
+#   Copyright 2018 Braxton Mckee
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+from typed_python import *
+from object_database.algebraic_protocol import AlgebraicProtocol
+
+import unittest
+import asyncio
+import threading
+import queue
+
+Message = Alternative(
+    "Message", 
+    Ping = {},
+    Pong = {}
+    )
+
+class PingPongProtocol(AlgebraicProtocol):
+    def __init__(self):
+        AlgebraicProtocol.__init__(self, Message, Message)
+
+    def messageReceived(self, m):
+        if m.matches.Ping:
+            self.sendMessage(Message.Pong())
+
+class SendAndReturn(AlgebraicProtocol):
+    def __init__(self, messageToSend, responseQueue):
+        AlgebraicProtocol.__init__(self, Message, Message)
+        self.messageToSend = messageToSend
+        self.responseQueue = responseQueue
+
+    def onConnected(self):
+        self.sendMessage(self.messageToSend)
+
+    def messageReceived(self, msg):
+        self.responseQueue.put(msg)
+        self.transport.close()
+
+class AlgebraicProtocolTests(unittest.TestCase):
+    def test_basic_send_and_receive(self):
+        loop = asyncio.get_event_loop()
+        
+        # Each client connection will create a new protocol instance
+        serverCoro = loop.create_server(PingPongProtocol, '127.0.0.1', 8888)
+
+        server = loop.run_until_complete(serverCoro)
+
+        q = queue.Queue()
+
+        clientCoro = loop.create_connection(
+            lambda: SendAndReturn(Message.Ping(), q),
+            '127.0.0.1', 
+            8888
+            )
+        loop.run_until_complete(clientCoro)
+
+        self.assertEqual(q.get(timeout=1.0), Message.Pong())
+
+        server.close()
+        loop.run_until_complete(server.wait_closed())
+        loop.close()
