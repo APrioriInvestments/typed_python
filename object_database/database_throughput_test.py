c0489450820dd676cde33454488b86080b9ea6da
Braxton Mckee <braxton@aprioriinvestments.com>
2018-08-14 12:00:59 -0400

    Add threads to the db-throughput test.

--- object_database/database_throughput_test.py
+++ object_database/database_throughput_test.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+import threading
 import argparse
 import sys
 import time
@@ -10,6 +10,7 @@ def main(argv):
 
     parser.add_argument("host")
     parser.add_argument("port")
+    parser.add_argument("--threads", default=1, type=int)
 
     parsedArgs = parser.parse_args(argv[1:])
 
@@ -19,25 +20,36 @@ def main(argv):
     class Counter:
         k = int
 
-    with db.transaction():
-        c = Counter.New()
+    def worker():
+        with db.transaction():
+            c = Counter.New()
+
+        while True:
+            t0 = time.time()
+            
+            with db.transaction():
+                c.k = 0
+
+            while time.time() - t0 < 1.0:
+                with db.transaction():
+                    c.k = c.k + 1
+
+            with db.view():
+                print(c.k, " transactions per second")
+
+    threads = [threading.Thread(target=worker) for _ in range(parsedArgs.threads)]
+    for t in threads:
+        t.daemon = True
+        t.start()
 
     while True:
-        t0 = time.time()
-        
-        with db.transaction():
-            c.k = 0
-
-        while time.time() - t0 < 1.0:
-            with db.transaction():
-                c.k = c.k + 1
-
-        with db.view():
-            print(c.k, " transactions per second")
+        time.sleep(0.01)
 
 if __name__ == '__main__':
     try:
     	main(sys.argv)
+    except KeyboardInterrupt:
+        pass
     except:
     	import traceback
     	traceback.print_exc()
