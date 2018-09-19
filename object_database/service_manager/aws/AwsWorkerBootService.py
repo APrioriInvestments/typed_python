5fc35a39fae1f1e6fc254de8881e1c73cd7a4561
Braxton Mckee <braxton@aprioriinvestments.com>
2018-09-19 15:02:45 -0400

    Track when aws worker booter hits the amazon instance limit.

--- object_database/service_manager/aws/AwsWorkerBootService.py
+++ object_database/service_manager/aws/AwsWorkerBootService.py
@@ -27,34 +27,154 @@ import traceback
 import datetime
 import os
 
+from typed_python import OneOf
 from object_database import ServiceBase, service_schema, Schema, Indexed
 from object_database.web import cells
 from nativepython.python.string_util import closest_N_in
 
 schema = Schema("core.AwsWorkerBootService")
 
-valid_instance_types = (
-    "t2.nano", "t2.micro", "t2.small", "t2.medium", "t2.large", 
-    "t2.xlarge", "t2.2xlarge", "t3.nano", "t3.micro", "t3.small", 
-    "t3.medium", "t3.large", "t3.xlarge", "t3.2xlarge", "m4.large", 
-    "m4.xlarge", "m4.2xlarge", "m4.4xlarge", "m4.10xlarge", "m4.16xlarge", 
-    "m5.large", "m5.xlarge", "m5.2xlarge", "m5.4xlarge", "m5.12xlarge", 
-    "m5.24xlarge", "m5d.large", "m5d.xlarge", 
-    "m5d.2xlarge", "m5d.4xlarge", "m5d.12xlarge", "m5d.24xlarge"
-    "c4.large", "c4.xlarge", "c4.2xlarge", "c4.4xlarge", "c4.8xlarge", 
-    "c5.large", "c5.xlarge", "c5.2xlarge", "c5.4xlarge", "c5.9xlarge", 
-    "c5.18xlarge", "c5d.xlarge", "c5d.2xlarge", "c5d.4xlarge", "c5d.9xlarge", "c5d.18xlarge"
-    "r4.large", "r4.xlarge", "r4.2xlarge", "r4.4xlarge", "r4.8xlarge", 
-    "r4.16xlarge", "r5.large", "r5.xlarge", "r5.2xlarge", "r5.4xlarge", 
-    "r5.12xlarge", "r5.24xlarge", "r5d.large", "r5d.xlarge", "r5d.2xlarge", 
-    "r5d.4xlarge", "r5d.12xlarge", "r5d.24xlarge", "x1.16xlarge", "x1.32xlarge", 
-    "x1e.xlarge", "x1e.2xlarge", "x1e.4xlarge", "x1e.8xlarge", "x1e.16xlarge", 
-    "x1e.32xlarge", "z1d.large", "z1d.xlarge", 
-    "z1d.2xlarge", "z1d.3xlarge", "z1d.6xlarge", "z1d.12xlarge"
-    "d2.xlarge", "d2.2xlarge", "d2.4xlarge", "d2.8xlarge", "h1.2xlarge", 
-    "h1.4xlarge", "h1.8xlarge", "h1.16xlarge", "i3.large", "i3.xlarge", 
-    "i3.2xlarge", "i3.4xlarge", "i3.8xlarge", "i3.16xlarge"
-    )
+valid_instance_types = {
+    'm1.small': {'RAM':1.7,'CPU':1,'COST':0.044},
+    'm1.medium': {'RAM':3.75,'CPU':1,'COST':0.087},
+    'm1.large': {'RAM':7.5,'CPU':2,'COST':0.175},
+    'm1.xlarge': {'RAM':15,'CPU':4,'COST':0.35},
+    'c1.medium': {'RAM':1.7,'CPU':2,'COST':0.13},
+    'c1.xlarge': {'RAM':7,'CPU':8,'COST':0.52},
+    'cc2.8xlarge': {'RAM':60.5,'CPU':32,'COST':2},
+    'm2.xlarge': {'RAM':17.1,'CPU':2,'COST':0.245},
+    'm2.2xlarge': {'RAM':34.2,'CPU':4,'COST':0.49},
+    'm2.4xlarge': {'RAM':68.4,'CPU':8,'COST':0.98},
+    'hs1.8xlarge': {'RAM':117,'CPU':16,'COST':4.6},
+    'm5.large': {'RAM':8,'CPU':2,'COST':0.096},
+    'm5.xlarge': {'RAM':16,'CPU':4,'COST':0.192},
+    'm5.2xlarge': {'RAM':32,'CPU':8,'COST':0.384},
+    'm5.4xlarge': {'RAM':64,'CPU':16,'COST':0.768},
+    'm5.12xlarge': {'RAM':192,'CPU':48,'COST':2.304},
+    'm5.24xlarge': {'RAM':384,'CPU':96,'COST':4.608},
+    'm4.large': {'RAM':8,'CPU':2,'COST':0.1},
+    'm4.xlarge': {'RAM':16,'CPU':4,'COST':0.2},
+    'm4.2xlarge': {'RAM':32,'CPU':8,'COST':0.4},
+    'm4.4xlarge': {'RAM':64,'CPU':16,'COST':0.8},
+    'm4.10xlarge': {'RAM':160,'CPU':40,'COST':2},
+    'm4.16xlarge': {'RAM':256,'CPU':64,'COST':3.2},
+    'c5.large': {'RAM':4,'CPU':2,'COST':0.085},
+    'c5.xlarge': {'RAM':8,'CPU':4,'COST':0.17},
+    'c5.2xlarge': {'RAM':16,'CPU':8,'COST':0.34},
+    'c5.4xlarge': {'RAM':32,'CPU':16,'COST':0.68},
+    'c5.9xlarge': {'RAM':72,'CPU':36,'COST':1.53},
+    'c5.18xlarge': {'RAM':144,'CPU':72,'COST':3.06},
+    'c4.large': {'RAM':3.75,'CPU':2,'COST':0.1},
+    'c4.xlarge': {'RAM':7.5,'CPU':4,'COST':0.199},
+    'c4.2xlarge': {'RAM':15,'CPU':8,'COST':0.398},
+    'c4.4xlarge': {'RAM':30,'CPU':16,'COST':0.796},
+    'c4.8xlarge': {'RAM':60,'CPU':36,'COST':1.591},
+    'r5.large': {'RAM':16,'CPU':2,'COST':0.126},
+    'r5.xlarge': {'RAM':32,'CPU':4,'COST':0.252},
+    'r5.2xlarge': {'RAM':64,'CPU':8,'COST':0.504},
+    'r5.4xlarge': {'RAM':128,'CPU':16,'COST':1.008},
+    'r5.12xlarge': {'RAM':384,'CPU':48,'COST':3.024},
+    'r5.24xlarge': {'RAM':768,'CPU':96,'COST':6.047},
+    'r4.large': {'RAM':15.25,'CPU':2,'COST':0.133},
+    'r4.xlarge': {'RAM':30.5,'CPU':4,'COST':0.266},
+    'r4.2xlarge': {'RAM':61,'CPU':8,'COST':0.532},
+    'r4.4xlarge': {'RAM':122,'CPU':16,'COST':1.064},
+    'r4.8xlarge': {'RAM':244,'CPU':32,'COST':2.128},
+    'r4.16xlarge': {'RAM':488,'CPU':64,'COST':4.256},
+    'p3.2xlarge': {'RAM':61,'CPU':8,'COST':3.06},
+    'p3.8xlarge': {'RAM':244,'CPU':32,'COST':12.24},
+    'p3.16xlarge': {'RAM':488,'CPU':64,'COST':24.48},
+    'p2.xlarge': {'RAM':61,'CPU':4,'COST':0.9},
+    'p2.8xlarge': {'RAM':488,'CPU':32,'COST':7.2},
+    'p2.16xlarge': {'RAM':732,'CPU':64,'COST':14.4},
+    'g3.4xlarge': {'RAM':122,'CPU':16,'COST':1.14},
+    'g3.8xlarge': {'RAM':244,'CPU':32,'COST':2.28},
+    'g3.16xlarge': {'RAM':488,'CPU':64,'COST':4.56},
+    'h1.2xlarge': {'RAM':32,'CPU':8,'COST':0.468},
+    'h1.4xlarge': {'RAM':64,'CPU':16,'COST':0.936},
+    'h1.8xlarge': {'RAM':128,'CPU':32,'COST':1.872},
+    'h1.16xlarge': {'RAM':256,'CPU':64,'COST':3.744},
+    'd2.xlarge': {'RAM':30.5,'CPU':4,'COST':0.69},
+    'd2.2xlarge': {'RAM':61,'CPU':8,'COST':1.38},
+    'd2.4xlarge': {'RAM':122,'CPU':16,'COST':2.76},
+    'd2.8xlarge': {'RAM':244,'CPU':36,'COST':5.52},
+    'm3.medium': {'RAM':3.75,'CPU':1,'COST':0.067},
+    'm3.large': {'RAM':7.5,'CPU':2,'COST':0.133},
+    'm3.xlarge': {'RAM':15,'CPU':4,'COST':0.266},
+    'm3.2xlarge': {'RAM':30,'CPU':8,'COST':0.532},
+    'c3.large': {'RAM':3.75,'CPU':2,'COST':0.105},
+    'c3.xlarge': {'RAM':7.5,'CPU':4,'COST':0.21},
+    'c3.2xlarge': {'RAM':15,'CPU':8,'COST':0.42},
+    'c3.4xlarge': {'RAM':30,'CPU':16,'COST':0.84},
+    'c3.8xlarge': {'RAM':60,'CPU':32,'COST':1.68},
+    'g2.2xlarge': {'RAM':15,'CPU':8,'COST':0.65},
+    'g2.8xlarge': {'RAM':60,'CPU':32,'COST':2.6},
+    'cr1.8xlarge': {'RAM':244,'CPU':32,'COST':3.5},
+    'x1.16xlarge': {'RAM':976,'CPU':64,'COST':6.669},
+    'x1.32xlarge': {'RAM':1952,'CPU':128,'COST':13.338},
+    'x1e.xlarge': {'RAM':122,'CPU':4,'COST':0.834},
+    'x1e.2xlarge': {'RAM':244,'CPU':8,'COST':1.668},
+    'x1e.4xlarge': {'RAM':488,'CPU':16,'COST':3.336},
+    'x1e.8xlarge': {'RAM':976,'CPU':32,'COST':6.672},
+    'x1e.16xlarge': {'RAM':1952,'CPU':64,'COST':13.344},
+    'x1e.32xlarge': {'RAM':3904,'CPU':128,'COST':26.688},
+    'r3.large': {'RAM':15.25,'CPU':2,'COST':0.166},
+    'r3.xlarge': {'RAM':30.5,'CPU':4,'COST':0.333},
+    'r3.2xlarge': {'RAM':61,'CPU':8,'COST':0.665},
+    'r3.4xlarge': {'RAM':122,'CPU':16,'COST':1.33},
+    'r3.8xlarge': {'RAM':244,'CPU':32,'COST':2.66},
+    'i2.xlarge': {'RAM':30.5,'CPU':4,'COST':0.853},
+    'i2.2xlarge': {'RAM':61,'CPU':8,'COST':1.705},
+    'i2.4xlarge': {'RAM':122,'CPU':16,'COST':3.41},
+    'i2.8xlarge': {'RAM':244,'CPU':32,'COST':6.82},
+    'm5d.large': {'RAM':8,'CPU':2,'COST':0.113},
+    'm5d.xlarge': {'RAM':16,'CPU':4,'COST':0.226},
+    'm5d.2xlarge': {'RAM':32,'CPU':8,'COST':0.452},
+    'm5d.4xlarge': {'RAM':64,'CPU':16,'COST':0.904},
+    'm5d.12xlarge': {'RAM':192,'CPU':48,'COST':2.712},
+    'm5d.24xlarge': {'RAM':384,'CPU':96,'COST':5.424},
+    'c5d.large': {'RAM':4,'CPU':2,'COST':0.096},
+    'c5d.xlarge': {'RAM':8,'CPU':4,'COST':0.192},
+    'c5d.2xlarge': {'RAM':16,'CPU':8,'COST':0.384},
+    'c5d.4xlarge': {'RAM':32,'CPU':16,'COST':0.768},
+    'c5d.9xlarge': {'RAM':72,'CPU':36,'COST':1.728},
+    'c5d.18xlarge': {'RAM':144,'CPU':72,'COST':3.456},
+    'r5d.large': {'RAM':16,'CPU':2,'COST':0.144},
+    'r5d.xlarge': {'RAM':32,'CPU':4,'COST':0.288},
+    'r5d.2xlarge': {'RAM':64,'CPU':8,'COST':0.576},
+    'r5d.4xlarge': {'RAM':128,'CPU':16,'COST':1.152},
+    'r5d.12xlarge': {'RAM':384,'CPU':48,'COST':3.456},
+    'r5d.24xlarge': {'RAM':768,'CPU':96,'COST':6.912},
+    'z1d.large': {'RAM':16,'CPU':2,'COST':0.186},
+    'z1d.xlarge': {'RAM':32,'CPU':4,'COST':0.372},
+    'z1d.2xlarge': {'RAM':64,'CPU':8,'COST':0.744},
+    'z1d.3xlarge': {'RAM':96,'CPU':12,'COST':1.116},
+    'z1d.6xlarge': {'RAM':192,'CPU':24,'COST':2.232},
+    'z1d.12xlarge': {'RAM':384,'CPU':48,'COST':4.464},
+    'f1.2xlarge': {'RAM':122,'CPU':8,'COST':1.65},
+    'f1.16xlarge': {'RAM':976,'CPU':64,'COST':13.2},
+    'i3.large': {'RAM':15.25,'CPU':2,'COST':0.156},
+    'i3.xlarge': {'RAM':30.5,'CPU':4,'COST':0.312},
+    'i3.2xlarge': {'RAM':61,'CPU':8,'COST':0.624},
+    'i3.4xlarge': {'RAM':122,'CPU':16,'COST':1.248},
+    'i3.8xlarge': {'RAM':244,'CPU':32,'COST':2.496},
+    'i3.16xlarge': {'RAM':488,'CPU':64,'COST':4.992}
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 @schema.define
 class Configuration:
@@ -78,6 +198,8 @@ class State:
 
     booted = int
     desired = int
+    observedLimit = OneOf(None, int) #maximum observed limit count
+    capacityConstrained = bool
 
 own_dir = os.path.split(__file__)[0]
 
@@ -312,8 +434,12 @@ class AwsWorkerBootService(ServiceBase):
 
     def doWork(self, shouldStop):
         while not shouldStop.is_set():
-            if not self.pushTaskLoopForward():
-                time.sleep(1.0)
+            try:
+                if not self.pushTaskLoopForward():
+                    time.sleep(1.0)
+            except:
+                logging.error("Failed: %s", traceback.format_exc())
+                time.sleep(5.0)
 
 
 
@@ -333,7 +459,7 @@ class AwsWorkerBootService(ServiceBase):
             return f
 
         return cells.Grid(
-            colFun=lambda: ['Instance Type', 'Booted', 'Desired'],
+            colFun=lambda: ['Instance Type', 'COST', 'RAM', 'CPU', 'Booted', 'Desired','ObservedLimit', 'CapacityConstrained'],
             rowFun=lambda: sorted(State.lookupAll(), key=lambda s:s.instance_type),
             headerFun=lambda x: x,
             rowLabelFun=None,
@@ -342,6 +468,11 @@ class AwsWorkerBootService(ServiceBase):
                 s.booted  if field == 'Booted' else 
                 cells.Dropdown(str(s.desired), [(str(ct), bootCountSetter(s, ct)) for ct in list(range(10)) + list(range(10,101,10))]) 
                         if field == 'Desired' else 
+                ("" if s.observedLimit is None else s.observedLimit) if field == 'ObservedLimit' else 
+                ("Yes" if s.capacityConstrained else "") if field == 'CapacityConstrained' else 
+                valid_instance_types[s.instance_type]['COST'] if field == 'COST' else 
+                valid_instance_types[s.instance_type]['RAM'] if field == 'RAM' else 
+                valid_instance_types[s.instance_type]['CPU'] if field == 'CPU' else 
                 ""
                 )
             ) + cells.Card(
@@ -407,10 +538,20 @@ class AwsWorkerBootService(ServiceBase):
                         self.api.bootWorker(state.instance_type)
 
                         state.booted += 1
-                    except:
-                        logging.error("Failed to boot a worker:\n%s", traceback.format_exc())
-                        time.sleep(self.SLEEP_INTERVAL)
-                        break
+                        state.capacityConstrained = False
+                    except Exception as e:
+                        if 'InsufficientInstanceCapacity' in str(e):
+                            state.desired = state.booted
+                            state.capacityConstrained = True
+                        elif 'You have requested more instances ' in str(e):
+                            maxCount = int(str(e).split('than your current instance limit of ')[1].split(' ')[0])
+                            logging.info("Visible limit of %s observed for instance type %s", maxCount, state.instance_type)
+                            state.observedLimit = maxCount
+                            state.desired = min(state.desired, maxCount)
+                        else:
+                            logging.error("Failed to boot a worker:\n%s", traceback.format_exc())
+                            time.sleep(self.SLEEP_INTERVAL)
+                            break
 
         time.sleep(self.SLEEP_INTERVAL)
         
