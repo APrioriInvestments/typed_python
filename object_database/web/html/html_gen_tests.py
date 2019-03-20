134192f9fb53717f2cf055aa172979e44be33ac3
dkrasner <krasnerdaniel@gmail.com>
2019-03-20 12:58:33 +0100

    adding further tests to html_get_tests

--- object_database/web/html/html_gen_tests.py
+++ object_database/web/html/html_gen_tests.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 
-from html_gen import *
+import re
+import unittest
+
+from html_gen import HTMLElement, HTML_TAG_CONFIG, HTMLElementChildrenError
 from io import StringIO
 
-import unittest
 
 class HTMLGeneratorTests(unittest.TestCase):
     def setUp(self):
-        pass
+        self.current_tag_names = [item["tag_name"] for item in HTML_TAG_CONFIG]
 
     def test_add_child(self):
         test_child = HTMLElement()
@@ -32,13 +34,28 @@ class HTMLGeneratorTests(unittest.TestCase):
                 all_present = False
         self.assertTrue(all_present)
 
-    def test_print_on(self):
+    def test_print_on_basic(self):
         stream = StringIO()
         element = HTMLElement('div')
         element.attributes['class'] = 'column-4 medium'
         element.print_on(stream, newlines=False)
         output = stream.getvalue()
         self.assertEqual('<div class="column-4 medium"></div>', output)
+
+    def test_print_on_nested(self):
+        stream = StringIO()
+        child = HTMLElement('p')
+        child.attributes['class'] = 'column-4 medium'
+        parent = HTMLElement('div', children=[child])
+        parent.attributes['class'] = 'column-4 medium'
+        parent.print_on(stream, newlines=False)
+        output = stream.getvalue()
+        test_out =('<div class="column-4 medium"><p class="column-4 medium">' +
+                  '</p></div>')
+        # we don't care about white spaces or new linesso much
+        output = re.sub('\s{2,}', '', output)
+        output = re.sub(r'\n', '', output)
+        self.assertEqual(test_out, output)
 
     def test_print_on_set_boundmethod(self):
         stream = StringIO()
@@ -47,6 +64,11 @@ class HTMLGeneratorTests(unittest.TestCase):
         element.print_on(stream, newlines=False)
         output = stream.getvalue()
         self.assertEqual('<div class="column-4 medium"></div>', output)
+
+    def test_list_methods(self):
+        method_names = HTMLElement.all_methods
+        for name in self.current_tag_names:
+            self.assertIn(name, method_names)
 
     def tearDown(self):
         pass
