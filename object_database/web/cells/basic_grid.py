d26f41d50873396528bc3e98a924fcfa679e3d97
Eric Gade <eric.gade@gmail.com>
2019-04-26 15:12:06 -0400

    Adding caveats to demo cells in comment

--- object_database/web/cells/basic_grid.py
+++ object_database/web/cells/basic_grid.py
@@ -1,3 +1,18 @@
+#   Copyright 2017-2019 Nativepython Authors
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+"""Example consumer of GridLayout"""
 from object_database.web.cells import Cell
 from object_database.web.html.html_gen import HTMLElement, HTMLTextContent
 from object_database.web.html.styles import StyleAttributes
@@ -35,6 +50,7 @@ class GridView(Cell):
         return StyleAttributes(height='80vh', width='80vw')
 
 class ColorCell(Cell):
+    """Just an example. Not for prod."""
     def __init__(self, color, children=[]):
         super().__init__()
         self.color = color
