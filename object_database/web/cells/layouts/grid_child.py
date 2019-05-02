ef85c86234c1474d503e6ef1800023ddbe181325
Eric Gade <eric.gade@gmail.com>
2019-05-01 20:17:03 -0400

    Adding comments

--- object_database/web/cells/layouts/grid_child.py
+++ object_database/web/cells/layouts/grid_child.py
@@ -12,13 +12,46 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-"""A ChildLayout object that represents styling for children of Grid containers.
-"""
 from object_database.web.html.styles import StyleAttributes
 
-class GridChildStyler():
+
+class GridChildStyler(object):
+    """A Styler for generating styles for children of GridLayouts.
+
+        This class represents a partial implementation of the
+        CSS Grid Specification as it pertains to child elements
+        of CSS Grid parent elements. For a more in-depth
+        explanation, see the MDN documentation at
+        https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Grid_Layout
+    """
     def __init__(self, row_start=None, row_end=None,
                  column_start=None, column_end=None, **kwargs):
+        """
+        Parameters
+        ----------
+        row_start: int
+            The row grid-line number at which this
+            child should start. Defaults to None.
+        row_end: int
+            The row grid-line number at which this
+            child should end. Defaults to None.
+        column_start: int
+            The column grid-line number at which
+            this child should start. Defaults to None.
+        column_end: int
+            The column grid-line number at which
+            this child should end. Defaults to None.
+        row_span: int
+            An alternative to row_end. Determines
+            how many cells to 'span' from the start
+            on the row axis. Will override any set
+            row_end value. Defaults to None.
+        column_span: int
+            An alternative to column_end. Determines
+            how many cells to 'span' from the start
+            on the column axis. Will override any
+            set column_end value. Defaults to None.
+        """
         self.row_start = row_start
         self.row_end = row_end
         self.column_start = column_start
@@ -31,12 +64,23 @@ class GridChildStyler():
             self.column_span = kwargs['column_span']
 
     def get_styles(self):
+        """Returns a configured StyleAttributes instance.
+
+        Returns
+        -------
+        StyleAttributes
+            A StyleAttributes instance configured
+            with the appropriate values for a
+            grid element child.
+        """
         column = self._make_column_styles()
         row = self._make_row_styles()
         return column + row
 
 
     def _make_column_styles(self):
+        """Returns a StyleAttributes instance for
+        the columns as configured"""
         styles = StyleAttributes()
         if self.column_end:
             styles.add_style('grid-column-end', str(self.column_end))
@@ -49,6 +93,8 @@ class GridChildStyler():
         return styles
 
     def _make_row_styles(self):
+        """Returns a StyleAttributes instance for
+        the rows as configured"""
         styles = StyleAttributes()
         if self.row_end:
             styles.add_style('grid-row-end', str(self.row_end))
