7373b558cc8a8892bfb2a04d82ebf29fc0513859
Eric Gade <eric.gade@gmail.com>
2019-05-14 13:05:45 -0400

    Updating replacements setting and patterns

--- object_database/web/content/components/Card.js
+++ object_database/web/content/components/Card.js
@@ -18,12 +18,18 @@
 class Card extends Component {
     constructor(props){
         super(props);
+        this.addReplacement('contents', '_____contents__');
     }
 
     render(){
-        return h('div', {class: "card", id: this.props.id}, [
+        return h('div',
+                 {
+                     class: "card",
+                     id: this.props.id,
+                     "data-cell-id": this.props.id
+                 }, [
             h('div', { class: "card-body p-1" }, [
-                h('div', {id: this.props.contentsId}, [])
+                h('div', {id: this.getReplacement('contents')}, [])
             ])
         ]);
     }
