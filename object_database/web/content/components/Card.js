d12bdc2c513760e3a17900ae6d5a89b6ab2fdef8
Eric Gade <eric.gade@gmail.com>
2019-05-14 12:35:01 -0400

    Initial commit of generic component

--- object_database/web/content/components/Card.js
+++ object_database/web/content/components/Card.js
@@ -1,8 +1,8 @@
 // Creates a button component
-function Card(id) {
+/*function Card(id) {
     // TODO: this is some hacky nonsense, but 
     let contents_id = id + "_____contents__";
-    
+
     var card = {
         render: function() {
             return h('div', {class: "card", id: id}, [
@@ -13,4 +13,18 @@ function Card(id) {
         }
     };
     return card;
+    }*/
+
+class Card extends Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return h('div', {class: "card", id: this.props.id}, [
+            h('div', { class: "card-body p-1" }, [
+                h('div', {id: this.props.contentsId}, [])
+            ])
+        ]);
+    }
 }
