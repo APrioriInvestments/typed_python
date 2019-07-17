5a539fd081d9553a62c4dccd6cb36b6188ab05f1
dkrasner <krasnerdaniel@gmail.com>
2019-07-17 18:11:49 -0400

    adding non working CollapsiblePanel to playground

---
+++ object_database/web/cells_demo/collapsible_panel.py
@@ -0,0 +1,28 @@
+#   Coyright 2017-2019 Nativepython Authors
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+from object_database.web import cells as cells
+from object_database.web.CellsTestPage import CellsTestPage
+
+
+class BasicCollapsiblePanel(CellsTestPage):
+    def cell(self):
+        return cells.CollapsiblePanel(
+            panel=cells.Card("I am a panel", padding=2),
+            content=cells.Card("I am some content"),
+            isExpanded=False
+        )
+
+    def text(self):
+        return "You should see a non-expanded collapsible panel."
