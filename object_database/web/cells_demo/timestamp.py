cd17c107016e228968ee8d7e05d9d8b7d0f0cc5d
dkrasner <krasnerdaniel@gmail.com>
2019-07-17 07:44:10 -0400

    Fix some bugs in the Timestamp display. Closes #193.

--- object_database/web/cells_demo/timestamp.py
+++ object_database/web/cells_demo/timestamp.py
@@ -13,15 +13,45 @@
 #   limitations under the License.
 
 import time
+from datetime import datetime
+import pytz
 
 from object_database.web import cells as cells
 from object_database.web.CellsTestPage import CellsTestPage
 
 
-class BasicTimestamp(CellsTestPage):
+class CurrentTimestamp(CellsTestPage):
     def cell(self):
         displayTime = time.time()
         return cells.Timestamp(displayTime)
 
     def text(self):
         return "You should see the current time and zone nicely displayed."
+
+
+class DefinedTimestamp(CellsTestPage):
+    def cell(self):
+        nyc = pytz.timezone("America/New_York")
+        timestamp = nyc.localize(datetime(2019, 7, 16, 0, 0, 0)).timestamp()
+
+        return cells.Sequence([
+            cells.Timestamp(timestamp + 3600 * i)
+            for i in range(24)]
+        )
+
+    def text(self):
+        return "You should see 24 hours of timestamps all correctly displayed with their hours."
+
+
+class Milliseconds(CellsTestPage):
+    def cell(self):
+        nyc = pytz.timezone("America/New_York")
+        timestamp = nyc.localize(datetime(2019, 7, 16, 8, 0, 0)).timestamp()
+
+        return cells.Sequence([
+            cells.Timestamp(timestamp + i / 1000)
+            for i in [1, 2, 10, 20, 100, 200]]
+        )
+
+    def text(self):
+        return "You should see '2019-07-16 08:00:00' followed by 1, 2, 10, 20, 100, 200 milliseconds."
