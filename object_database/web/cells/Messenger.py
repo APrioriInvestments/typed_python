592cd13e171b53ee2a946a59733fcfe9431bb421
dkrasner <krasnerdaniel@gmail.com>
2019-08-08 10:04:10 -0400

    removing all cellUpdate handler and related minor cleanup in cells.py

--- object_database/web/cells/Messenger.py
+++ object_database/web/cells/Messenger.py
@@ -5,42 +5,7 @@ should be formatted using functions in this module
 """
 
 
-def cellUpdated(cell, replaceDict={}):
-    """A lifecycle message formatter
-    to be used when a Cell is created or
-    updated.
-
-    Parameters
-    ----------
-    cell: Cell
-          An instance of a Cell whose view
-          should be updated in the UI
-    replaceDict: dict
-          A dictionary containing child cell ids
-          mapped to replacement string names
-
-    Returns
-    -------
-    A JSON parsable dictionary that can
-    be sent over a websocket
-    """
-    res = {
-        'channel': '#main',
-        'type': '#cellUpdated',
-        'id': cell.identity,
-        'replacements': replaceDict,
-        'shouldDisplay': cell.shouldDisplay,
-        'componentName': cell.__class__.__name__,
-        'replacementKeys': [k for k in cell.children.keys()],
-        'extraData': cell.exportData
-    }
-    if cell.postscript:
-        res['postscript'] = cell.postscript
-
-    return res
-
-
-def newCellUpdated(cell):
+def cellUpdated(cell):
     parent_id = None
     if cell.parent is not None:
         parent_id = cell.parent.identity
