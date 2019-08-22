e350dbb760677c33062010e8a21a06be3ba26618
Eric Gade <eric.gade@gmail.com>
2019-08-22 11:53:21 -0400

    Cleaning up and commenting NewCellHandler

--- object_database/web/content/NewCellHandler.js
+++ object_database/web/content/NewCellHandler.js
@@ -52,8 +52,6 @@ class NewCellHandler {
         this._updateComponentProps = this._updateComponentProps.bind(this);
         this._updateNamedChildren = this._updateNamedChildren.bind(this);
         this._findOrCreateChild = this._findOrCreateChild.bind(this);
-        this._removeFromParent = this._removeFromParent.bind(this);
-        this._removeAllChildren = this._removeAllChildren.bind(this);
         this._callDidLoadForNew = this._callDidLoadForNew.bind(this);
         this._callDidUpdate = this._callDidUpdate.bind(this);
     }
@@ -81,6 +79,19 @@ class NewCellHandler {
         ]);
     }
 
+    /**
+     * Main entrypoint into the handler.
+     * Consumers of this class should call
+     * only this method when receiving messages.
+     * It will case out the appropriate handling
+     * method based on the `type` field in the
+     * message.
+     * Note taht we call `doesNotUnderstand()`
+     * in the event of a message containing a
+     * message type that is unknown to the system.
+     * @param {Object} message - A JSON decoded
+     * message to be handled.
+     */
     receive(message){
         switch(message.type){
         case '#cellUpdated':
@@ -96,16 +107,48 @@ class NewCellHandler {
 
     /** Primary Message Handlers **/
 
+    /**
+     * Catch-all message handler for messages
+     * whose `type` is not understood by the
+     * system (ie has no appropriate handler)
+     * @param {Object} message - A JSON decoded
+     * message to be handled
+     */
     doesNotUnderstand(message){
         let msg = `CellHandler does not understand the following message: ${message}`;
         console.error(msg);
         return;
     }
 
+    /**
+     * Adds the messages's passed raw JS
+     * string to this instance's internal
+     * list of postscripts.
+     * @param {Object} message -  A JSON decoded
+     * message with `script` string contents.
+     */
     appendPostscript(message){
         this.postscripts.push(message.script);
     }
 
+    /**
+     * Primary handler for messages in which a given
+     * Cell/Component is either updated or initially
+     * created. These components will be added to
+     * a managed dictionary as they are created,
+     * so that we can keep track of instances for later
+     * updating purposes.
+     * This is where all of the DOM updating and
+     * manipulation occurs.
+     * Note the use of several helper functions.
+     * These were designed to deal wtih updating
+     * any namedChildren of the passed-in Cell/Component.
+     * @param {Object} message - A JSON decoded message
+     * containing a description of a Cell structure
+     * that should either be updated or created.
+     * @returns {Component} - A component that is configured
+     * and rendered, along with all of its children.
+     */
     cellUpdated(message){
         let component = this._getUpdatedComponent(message);
         let velement = render(component);
@@ -121,6 +164,22 @@ class NewCellHandler {
         return component;
     }
 
+    /**
+     * Message handler for the event of a Cell
+     * being discarded. We simply remove any corresponding
+     * component instance from the activeComponents
+     * managed dictionary.
+     * Note that a child being discarded from a parent
+     * is not handled here; rather, the #cellUpdated
+     * message sent for the parent will automatically
+     * re-render without the discarded child, so we
+     * don't need to do anything complex in this method.
+     * @param {Object} message - A JSON decoded message
+     * object containing the id of the Cell/Component
+     * that was discarded.
+     * @returns {Component} - The Component instance
+     * that was discarded.
+     */
     cellDiscarded(message){
         let found = this.activeComponents[message.id];
         if(found){
@@ -198,7 +257,25 @@ class NewCellHandler {
     }
 
 
-    /* Private Methods */
+    /** Private Methods **/
+
+    /**
+     * Attempts to find or create a component based
+     * on a description of the corresponding Cell
+     * present in a message object.
+     * If the id of the description is not in the
+     * current dictionary of activeComponents, we
+     * create it and proceed. Otherwise we find the
+     * component and update its props and children
+     * recursively.
+     * @param {Object} description - A description of the Cell
+     * that needs to be updated or created. This is usually a part
+     * of an incoming socket message object, and includes all of
+     * the information needed to properly create and render
+     * a cell component.
+     * @returns {Component} - A configured Component (with children)
+     * prepared for rendering.
+     */
     _getUpdatedComponent(description){
         if(!Object.keys(this.activeComponents).includes(description.id.toString())){
             // In this case, we are creating a totally new Cell
@@ -212,6 +289,17 @@ class NewCellHandler {
         return component;
     }
 
+    /**
+     * Handles the creation and registration of a new Component
+     * based on a Cell description in a message object.
+     * Note that other helpers might call this method
+     * recursively as it creates or updates any of its
+     * children.
+     * @param {Object} message - A JSON decoded message that
+     * contains a description of the Cell that should be
+     * created, along with props and children descriptions.
+     * @returns {Component} - The newly created Component
+     */
     _createAndUpdate(message){
         let componentClass = this.availableComponents[message.cellType];
         if(!componentClass || componentClass == undefined){
@@ -229,6 +317,19 @@ class NewCellHandler {
 
     }
 
+    /**
+     * Helper that will update the `props` of
+     * a given component based on a incoming
+     * #cellUpdated message.
+     * Note that we also call the component's
+     * `componentWillReceiveProps` lifecycle
+     * method here.
+     * @param {Component} component - The component
+     * whose props will be updated.
+     * @param {Object} message - A JSON decoded
+     * message containing a description of the Cell
+     * to be updated, including its new props.
+     */
     _updateComponentProps(component, message){
         let nextProps = Object.assign({}, message.extraData, {
             id: message.id,
@@ -241,6 +342,17 @@ class NewCellHandler {
         component._updateProps(newProps);
     }
 
+    /**
+     * Helper  that will update the namedChildren
+     * of the given component based on the updated
+     * namedChildren description in the provided
+     * #cellUpdated message object.
+     * @param {Component} component - The component
+     * whose namedChildren will be updated.
+     * @param {Object} message - A JSON decoded
+     * update message containing a description of
+     * the correpsonding Cell and its namedChildren.
+     */
     _updateNamedChildren(component, message){
         let newNamedChildren = {};
         Object.keys(message.namedChildren).forEach(childName => {
@@ -250,6 +362,22 @@ class NewCellHandler {
         component.props.namedChildren = newNamedChildren;
     }
 
+    /**
+     * Helper that will determine whether a component
+     * needs to be updated or created based upon a description
+     * of the cell and an instance of its parent Component.
+     * Note that this method is called recursively.
+     * Note also that this is where we set the `parent`
+     * property of the child component.
+     * @param {Object} childDescription - An object
+     * represending a description of the corresponding
+     * Cell, usually in the form of a #cellUpdated message
+     * @param {Component} parentComponent - A Component
+     * instance that represents the parent of the Component
+     * that will be updated or created.
+     * @returns {Component} childComponent - The found or
+     * created Component instance.
+     */
     _findOrCreateChild(childDescription, parentComponent){
         if(Array.isArray(childDescription)){
             return childDescription.map(item => {
@@ -261,34 +389,12 @@ class NewCellHandler {
         return childComponent;
     }
 
-    _removeFromParent(component){
-        let search = findNamedComponent(component, component.parent);
-        if(search.found == false){
-            console.warn(`Could not find ${component.name}(${component.props.id}) in parent's namedChildren`);
-            return false;
-        } else if(search.found && search.inArray){
-            // In this case, the result was in an array.
-            // So we need to find and splice that array.
-            let idx = search.inArray.indexOf(component);
-            if(idx < 0){
-                throw new Error(`${component.name}(${component.props.id}) removal failed trying to find itself in parent children using utility method.`);
-            }
-            search.inArray.splice(idx, 1);
-            return true;
-        } else if(search.found){
-            let parentNamedChildren = component.parent.props.namedChildren;
-            parentNamedChildren[search.nameInParent] = null;
-            return true;
-        }
-        return false;
-    }
-
-    _removeAllChildren(component){
-        allNamedChildrenDo(component, child => {
-            delete this.activeComponents[child.props.id];
-        });
-    }
-
+    /**
+     * Helper that cucles through all components
+     * that were freshly created as a part of a
+     * single message handling cycle and calls the
+     * `componentDidLoad` lifecycle method on each.
+     */
     _callDidLoadForNew(){
         // Goes through the stored list of
         // new components created during a message handle
@@ -299,6 +405,13 @@ class NewCellHandler {
         this._newComponents = [];
     }
 
+    /**
+     * Helper that cycles through all Components
+     * that were updated (but not created) as a
+     * part of a single message handling cycle
+     * and calls the `componentDidUpdate`
+     * lifecycle method on each.
+     */
     _callDidUpdate(){
         // Goes through the stored
         // list of existing components
