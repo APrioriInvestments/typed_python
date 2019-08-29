24e94f1821d4171ff3471d927b6bb99b9d52b115
Eric Gade <eric.gade@gmail.com>
2019-08-29 15:55:26 -0400

    First pass at implementing new vert Flex behavior with examples

--- object_database/web/cells/__init__.py
+++ object_database/web/cells/__init__.py
@@ -71,6 +71,8 @@ from object_database.web.cells.CellsTestMixin import CellsTestMixin
 
 from object_database.web.cells.util import waitForCellsCondition
 
+from object_database.web.cells.util import Flex, ShrinkWrap
+
 from object_database.web.cells.views.resizable_panel import ResizablePanel
 
 MAX_FPS = 10
