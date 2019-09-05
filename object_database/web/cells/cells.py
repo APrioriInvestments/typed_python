f14fdae9cad3689ed5b7164e1853277237a705bb
Eric Gade <eric.gade@gmail.com>
2019-09-05 14:30:37 -0400

    Updating HorizontalSequence tests, flattening, and styling to use flex-wrap

--- object_database/web/cells/cells.py
+++ object_database/web/cells/cells.py
@@ -1146,7 +1146,7 @@ class Sequence(Cell):
 
 
 class HorizontalSequence(Cell):
-    def __init__(self, elements, overflow=True, margin=None):
+    def __init__(self, elements, overflow=True, margin=None, wrap=True):
         """
         Lays out (children) elements in a horizontal sequence.
 
@@ -1165,6 +1165,7 @@ class HorizontalSequence(Cell):
         self.elements = elements
         self.overflow = overflow
         self.margin = margin
+        self.wrap = wrap
         self.isFlexParent = False
         self.updateChildren()
 
@@ -1181,6 +1182,7 @@ class HorizontalSequence(Cell):
             self.exportData['flexParent'] = True
         self.exportData['overflow'] = self.overflow
         self.exportData['margin'] = self.margin
+        self.exportData['wrap'] = self.wrap
 
     def updateChildren(self):
         newElements = []
