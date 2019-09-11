8c8190cde25b814d6487a616d20a2c90f471282d
Eric Gade <eric.gade@gmail.com>
2019-09-11 17:20:44 -0400

    Fixing flex-parent/flex-child sequences styling bug

--- object_database/web/cells_demo/flex_subscribed_sequence.py
+++ object_database/web/cells_demo/flex_subscribed_sequence.py
@@ -17,16 +17,31 @@ from object_database.web.CellsTestPage import CellsTestPage
 from object_database.web.cells.util import Flex
 
 
-class VerticalSubscribedSequenceExample(CellsTestPage):
+class VerticalSubscribedSequenceNotFlex(CellsTestPage):
     def cell(self):
         x = cells.Slot(())
-        top_button = cells.Button("Add Item", lambda: x.set(x.get() + (len(x.get()))))
+        top_button = cells.Button("Add Item", lambda: x.set(x.get() + (len(x.get()),)))
         bottom_button = cells.Button("This does nothing", lambda: None)
         sub_sequence = cells.SubscribedSequence(
-            lambda: x.get,
+            lambda: x.get(),
             lambda item: cells.Text("Item: {}".format(item))
         )
         return (top_button + sub_sequence + bottom_button)
 
     def text(self):
-        return "Should display a vertical sequence that is flexed"
+        return "Should display a vertical sequence that is NOT flexed"
+
+
+class VerticalSubscribedSequenceFlexed(CellsTestPage):
+    def cell(self):
+        x = cells.Slot(())
+        top_button = cells.Button("Add Item", lambda: x.set(x.get() + (len(x.get()),)))
+        bottom_button = cells.Button("This does nothing", lambda: None)
+        sub_sequence = cells.SubscribedSequence(
+            lambda: x.get(),
+            lambda item: cells.Text("Item: {}".format(item))
+        )
+        return (top_button + Flex(sub_sequence) + bottom_button)
+
+    def text(self):
+        return "Should display a vertical sequence that is NOT flexed"
