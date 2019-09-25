261edca94ef61c00d633e10be68701a1074b8c49
Braxton Mckee <braxton@aprioriinvestments.com>
2019-09-25 10:17:10 -0400

    Fix 'Traceback' and add a demo for it.

--- object_database/web/content/components/Traceback.js
+++ object_database/web/content/components/Traceback.js
@@ -6,23 +6,12 @@ import {Component} from './Component';
 import {h} from 'maquette';
 
 /**
- * About Replacements
- * ------------------
- * This component has a single regular
- * repalcement:
- * * `child`
- */
-
-/**
  * About Named Children
  * `traceback` (single) - The cell containing the traceback text
  */
 class  Traceback extends Component {
     constructor(props, ...args){
         super(props, ...args);
-
-        // Bind component methods
-        this.makeTraceback = this.makeTraceback.bind(this);
     }
 
     build(){
@@ -31,17 +20,9 @@ class  Traceback extends Component {
                 id: this.props.id,
                 "data-cell-id": this.props.id,
                 "data-cell-type": "Traceback",
-                class: "alert alert-primary"
-            }, [this.getReplacementElementFor('child')])
+                class: "alert alert-primary traceback"
+            }, [this.renderChildNamed('traceback')])
         );
-    }
-
-    makeTraceback(){
-        if(this.usesReplacements){
-            return this.getReplacementElementFor('child');
-        } else {
-            return this.renderChildNamed('traceback');
-        }
     }
 }
 
