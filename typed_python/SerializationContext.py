29a4f3453ce1902c302cd09f98f01275a8929d78
Braxton Mckee <braxton@aprioriinvestments.com>
2022-11-10 08:28:41 -0500

    Fix a bug in SerializationContext that could cause deadlocks

--- typed_python/SerializationContext.py
+++ typed_python/SerializationContext.py
@@ -336,7 +336,12 @@ class SerializationContext:
                         if moduleName in _badModuleCache or not allowImport:
                             return None
 
-                        module = importlib.import_module(moduleName)
+                        module = None
+
+                # make sure not to import the module under lock
+                if module is None:
+                    module = importlib.import_module(moduleName)
+
             except ModuleNotFoundError:
                 _badModuleCache.add(moduleName)
                 return None
@@ -402,10 +407,12 @@ class SerializationContext:
                     if not allowImport:
                         return None
 
-                # make sure to import outside of the lock
-                # otherwise we can deadlock since importing
-                # may trigger compilation
-            module = importlib.import_module(moduleName)
+                    module = None
+            # make sure to import outside of the lock
+            # otherwise we can deadlock since importing
+            # may trigger compilation
+            if module is None:
+                module = importlib.import_module(moduleName)
 
             return getattr(module, objName, None)
         except ModuleNotFoundError:
