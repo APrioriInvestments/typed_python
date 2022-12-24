35266e411629205d1c75543dafbbd5e9a9714c57
Aaron Levy <aaron.levy@aprioriinvestments.com>
2022-12-24 14:47:33 +0000

    Add TimeZone.datetime

--- typed_python/lib/datetime/DateTime.py
+++ typed_python/lib/datetime/DateTime.py
@@ -1,23 +1,30 @@
 from typed_python import Class, Final, Member, Entrypoint
 from typed_python.lib.datetime.chrono import Chrono
 
+class NonexistentDateTime(Exception):
+    pass
 
 class TimeOfDay(Class, Final):
     hour = Member(int)
     minute = Member(int)
     second = Member(float)
 
-    def secondsSinceMidnight(self, occurrence: int=1) -> float:
-        if occurrence != 1:
-            raise NotImplementedError("Have to figure out API for repeat times")
+    def secondsSinceMidnight(self, afterFold: bool=False) -> float:
+        return (self.second + self.minute * 60 + self.hour * 3600) + afterFold * 3600
 
-        return (self.second + self.minute * 60 + self.hour * 3600) + 60
+    @Entrypoint
+    def __eq__(self, other):
+        return self.hour == other.hour and self.minute == other.minute and self.second == other.second
 
 
 class Date(Class, Final):
     year = Member(int)
     month = Member(int)
     day = Member(int)
+
+    @Entrypoint
+    def __eq__(self, other):
+        return self.year == other.year and self.month == other.month and self.day == other.day
 
     @Entrypoint
     def daysSinceEpoch(self):
@@ -39,11 +46,14 @@ class DateTime(Class, Final):
     date = Member(Date)
     timeOfDay = Member(TimeOfDay)
 
+    def __eq__(self, other):
+        return self.date == other.date and self.timeOfDay == other.timeOfDay
+
 
 
 class TimeZone(Class):
     @Entrypoint
-    def timestamp(dateTime: DateTime, occurrence: int = 1) -> float:
+    def timestamp(dateTime: DateTime, afterFold: bool = False) -> float:
         raise NotImplementedError("Subclasses implement.")
 
 
@@ -52,7 +62,7 @@ class DaylightSavingsTimezone(TimeZone, Final):
     st_offset_hours = Member(int)
 
     @Entrypoint
-    def timestamp(self, dateTime: DateTime, occurrence: int = 1) -> float:
+    def timestamp(self, dateTime: DateTime, afterFold: bool = False) -> float:
 
         year = dateTime.date.year
 
@@ -73,14 +83,46 @@ class DaylightSavingsTimezone(TimeZone, Final):
             is_daylight_savings = False
 
         if day == ds_start:
-            is_daylight_savings = dateTime.timeOfDay.hour>2
+            if dateTime.timeOfDay.hour == 2:
+                raise NonexistentDateTime("This date time does not exist on this date.")
+
+            is_daylight_savings = dateTime.timeOfDay.hour > 2
 
         if day == ds_end:
-            is_daylight_savings = occurrence < 2 and dateTime.timeOfDay.hour<2
+            if dateTime.timeOfDay.hour > 2:
+                afterFold = True
+                is_daylight_savings = True
+            else:
+                is_daylight_savings = afterFold < 2 and dateTime.timeOfDay.hour < 2
 
         offset_hours = self.dst_offset_hours if is_daylight_savings else self.st_offset_hours
 
-        return day * 86400 + offset_hours * 3600 + dateTime.timeOfDay.secondsSinceMidnight(occurrence)
+        return day * 86400 + offset_hours * 3600 + dateTime.timeOfDay.secondsSinceMidnight(afterFold)
+
+    @Entrypoint
+    def datetime(self, timestamp) -> DateTime:
+        daysSinceEpoch, secondsSinceMidnight = divmod(timestamp, 86400)
+        utcDateNt = Chrono.civil_from_days(daysSinceEpoch)
+
+        # second sunday of march 
+        ds_start = Chrono.get_nth_dow_of_month(2, 0, 3, utcDateNt.year) + self.st_offset_hours * 3600
+
+        # first sunday of november
+        ds_end = Chrono.get_nth_dow_of_month(1, 0, 11, utcDateNt.year) + self.dst_offset_hours * 3600
+
+        # get offset
+        offset_hours = self.dst_offset_hours if timestamp < ds_start and timestamp > ds_end else self.st_offset_hours
+
+        # adjust seconds since midnight for timezone and re-compute into datetime.
+        secondsSinceMidnight += offset_hours * 3600
+        daysToAdd, secondsSinceMidnight = divmod(secondsSinceMidnight, 86400)
+        hour, seconds = divmod(secondsSinceMidnight, 3600)
+        minute, second = divmod(seconds, 60)
+
+        date = Chrono.civil_from_days(daysSinceEpoch + daysToAdd) if daysToAdd else utcDateNt
+        tod = TimeOfDay(hour=hour, minute=minute, second=second)
+        return DateTime(date=Date(year=date.year, month=date.month, day=date.day), timeOfDay = tod)
+
 
 
 
@@ -88,5 +130,23 @@ class FixedOffsetTimezone(TimeZone, Final):
     offset_hours = Member(int)
 
     @Entrypoint
-    def timestamp(self, dateTime: DateTime, occurrence: int = 1) -> float:
-        return dateTime.date.daysSinceEpoch() * 86400 + dateTime.timeOfDay.secondsSinceMidnight(occurrence) + self.offset_hours * 3600
+    def timestamp(self, dateTime: DateTime, afterFold: bool = False) -> float:
+        return dateTime.date.daysSinceEpoch() * 86400 + dateTime.timeOfDay.secondsSinceMidnight(afterFold) + self.offset_hours * 3600
+
+    @Entrypoint
+    def datetime(self, timestamp) -> DateTime:
+        daysSinceEpoch, secondsSinceMidnight = divmod(timestamp, 86400)
+        utcDateNt = Chrono.civil_from_days(daysSinceEpoch)
+
+        offset_hours = self.offset_hours
+
+        # adjust seconds since midnight for timezone and re-compute into datetime.
+        secondsSinceMidnight += offset_hours * 3600
+        daysToAdd, secondsSinceMidnight = divmod(secondsSinceMidnight, 86400)
+        hour, seconds = divmod(secondsSinceMidnight, 3600)
+        minute, second = divmod(seconds, 60)
+
+        date = Chrono.civil_from_days(daysSinceEpoch + daysToAdd) if daysToAdd else utcDateNt
+        tod = TimeOfDay(hour=hour, minute=minute, second=second)
+        return DateTime(date=Date(year=date.year, month=date.month, day=date.day), timeOfDay = tod)
+
--- typed_python/lib/datetime/DateTime_test.py
+++ typed_python/lib/datetime/DateTime_test.py
@@ -1,10 +1,12 @@
+import pytest
 import pytz
 import datetime
-from typed_python.lib.datetime.DateTime import DateTime, Date, TimeOfDay, TimeZone, DaylightSavingsTimezone
+from typed_python.lib.datetime.DateTime import DateTime, Date, TimeOfDay, TimeZone, DaylightSavingsTimezone, NonexistentDateTime, FixedOffsetTimezone
 
 Nyc = DaylightSavingsTimezone(dst_offset_hours=4, st_offset_hours=5)
+Utc = FixedOffsetTimezone(offset_hours=0)
 
-def test_DateTime():
+def test_DateTime_to_timestamp():
     year, month, day = 2022, 12, 23
     hour, minute, second = 18, 40, 36
     date = Date(year=year, month=month, day=day)
@@ -18,4 +20,38 @@ def test_DateTime():
 
     assert timestamp == ts
 
+def test_DateTime_nonexistent_DateTime():
+    year, month, day = 2022, 3, 13
+    hour, minute, second = 2, 30, 0
+    date = Date(year=year, month=month, day=day)
+    timeOfDay = TimeOfDay(hour=hour, minute=minute, second=second)
+    dateTime = DateTime(date = date, timeOfDay=timeOfDay)
 
+    with pytest.raises(NonexistentDateTime):
+        Nyc.timestamp(dateTime)
+
+def test_DateTime_to_timestamp_daylight_savings():
+    year, month, day = 2022, 11, 6
+    hour, minute, second = 1, 30, 0
+    date = Date(year=year, month=month, day=day)
+    timeOfDay = TimeOfDay(hour=hour, minute=minute, second=second)
+    dateTime = DateTime(date = date, timeOfDay=timeOfDay)
+
+    timestamp1 = Nyc.timestamp(dateTime)
+    timestamp2 = Nyc.timestamp(dateTime, afterFold=True)
+
+    tz = pytz.timezone("America/New_York")
+    ts = tz.localize(datetime.datetime(year, month, day, hour, minute, second)).timestamp()
+
+    assert timestamp2 == ts
+
+def test_DateTime_from_timestamp():
+    year, month, day = 2022, 11, 6
+    hour, minute, second = 1, 30, 0
+    date = Date(year=year, month=month, day=day)
+    timeOfDay = TimeOfDay(hour=hour, minute=minute, second=second)
+    dateTime = DateTime(date = date, timeOfDay=timeOfDay)
+
+    timestamp = Utc.timestamp(dateTime)
+    newDateTime = Utc.datetime(timestamp)
+    assert newDateTime == dateTime
