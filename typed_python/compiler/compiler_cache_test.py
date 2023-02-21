60733cd647618c1beb100b95e7593019d9896f02
Augustus Lonergan <augustus.lonergan@aprioriinvestments.com>
2023-02-21 17:22:09 +0000

    nft wip multiprocess testing for compiler cache

--- typed_python/compiler/compiler_cache_test.py
+++ typed_python/compiler/compiler_cache_test.py
@@ -329,6 +329,59 @@ def test_can_compile_overlapping_code():
 
 
 @pytest.mark.skipif('sys.platform=="darwin"')
+def test_multiprocessing():
+    common = "\n".join([
+        "@Entrypoint",
+        "def f():",
+        "    return 1.0",
+
+        "@Entrypoint",
+        "def g():",
+        "    return f()",
+    ])
+
+    MODULES = {'common.py': common}
+
+    class Directory:
+        def __init__(self, tree):
+            self.tree = tree
+
+        @staticmethod
+        def fromPath(path):
+            tree = {}
+
+            def populate(path, tree):
+                for relPath in os.listdir(path):
+                    subPath = os.path.join(path, relPath)
+
+                    if os.path.isdir(subPath):
+                        populate(os.path.join(path, subPath), tree.setdefault(relPath, {}))
+                    elif os.path.isfile(subPath):
+                        with open(subPath, "rb") as file:
+                            tree[relPath] = file.read()
+
+            populate(path, tree)
+
+            return Directory(tree)
+
+    with tempfile.TemporaryDirectory() as dir1, tempfile.TemporaryDirectory() as dir2:
+        evaluateExprInFreshProcess(MODULES, "common.f()", dir1)
+
+        moduleName = os.listdir(dir1)[0]
+
+        from typed_python import SerializationContext
+
+        with open(dir1 + "/" + moduleName + "/" + "type_manifest.dat", "rb") as file:
+            print(SerializationContext().deserialize(file.read()))
+
+        thing = Directory.fromPath(dir1)
+
+        import pdb;pdb.set_trace()
+
+        pass
+
+
+@pytest.mark.skipif('sys.platform=="darwin"')
 def test_compiler_cache_handles_class_destructors_correctly():
     xmodule = "\n".join([
         "class C(Class):",
